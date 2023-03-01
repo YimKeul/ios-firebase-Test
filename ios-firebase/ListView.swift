b7734eb719cebe5281dcfe25e39777ff0e196fb6
YimKeul <leesjun29@gmail.com>
2023-03-02 00:01:45 +0900

    feat : firebase auth

---
+++ ios-firebase/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  ios-firebase
+//
+//  Created by yimkeul on 2023/03/01.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
--- ios-firebase/ios_firebaseApp.swift
+++ ios-firebase/ios_firebaseApp.swift
@@ -5,10 +5,33 @@
 //  Created by yimkeul on 2023/03/01.
 //
 
+
 import SwiftUI
+import Firebase
+//import FirebaseCore
+//import FirebaseFirestore
+//import FirebaseStorage
+//
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication,
+//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//
+//}
+//
+//let storage = Storage.storage()
 
 @main
 struct ios_firebaseApp: App {
+    // register app delegate for Firebase setup
+    //  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
