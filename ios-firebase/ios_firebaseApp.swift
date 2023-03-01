9df3a4beb3f6d3a344bdff7bcb67d49a0b209fcf
YimKeul <leesjun29@gmail.com>
2023-03-01 22:38:03 +0900

    Initial Commit

---
+++ ios-firebase/ios_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_firebaseApp.swift
+//  ios-firebase
+//
+//  Created by yimkeul on 2023/03/01.
+//
+
+import SwiftUI
+
+@main
+struct ios_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
